18045a340d59ae9d1d5391de2ea583c575c52a2f
Alex Sorokin <alex@crowdflower.com>
2014-07-02 22:57:31 -0700

    Move the apt_package patch to ros cookbook

--- providers/default.rb
+++ providers/default.rb
@@ -2,6 +2,25 @@ def whyrun_supported?
   true
 end
 
+## BEGIN HACK
+#
+# Until chef '11.14.0.rc.1' is out, we need this:
+# https://github.com/opscode/chef/commit/e9cfad2fd5c2c659e51fa7ef07906e1a80af7236#diff-956770fc4d43ca694d0f01b5414811ce
+#
+
+class Chef
+  class Provider
+    class Package
+      class Apt
+        def shell_out!( cmd, opts={} )
+          super(cmd, opts.merge(:timeout => 3600))
+        end
+      end
+    end
+  end
+end
+
+## END HACK
 action :install do
   converge_by("Add ROS apt repo") do
     apt_repository "ros-#{@new_resource.release}" do
